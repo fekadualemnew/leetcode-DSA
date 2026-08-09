1793500066adecdc521a8fe364b0d0d71373df56
fekadu <fekadu.alemnew27@gmail.com>
2026-08-09 17:28:18 +0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.36%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i > 0 and j > 0:
+                    if (matrix[i][j] != matrix[i -1][j -1]):
+                        return False     
+        return True
+    
+        
